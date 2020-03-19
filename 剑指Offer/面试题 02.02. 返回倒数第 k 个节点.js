e7cac9ef505216ed2dd698f2987445f392bdfd9e
ScarboroughCoral <3249977074@qq.com>
2020-03-19 19:45:46 +0800

    feat:添加剑指offer第02.02题，双指针

---
+++ 剑指Offer/面试题 02.02. 返回倒数第 k 个节点.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {number}
+ */
+var kthToLast = function (head, k) {
+  let fast = head, slow = head;
+  while (k--) {
+    fast = fast.next;
+  }
+  while (fast) fast = fast.next, slow = slow.next;
+  return slow.val;
+};
