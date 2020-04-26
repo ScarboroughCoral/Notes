717b8f91af26473159eabb25c95b7d188683ae71
ScarboroughCoral <3249977074@qq.com>
2020-04-26 22:30:26 +0800

    feat:添加LeetCode第23题，打卡题目

---
+++ LeetCode/23. 合并K个排序链表.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+    struct Node{
+        int val;
+        ListNode *p;
+        bool operator <(const Node& rhs) const {
+            return val>rhs.val;
+        }
+    };
+    priority_queue<Node> q;
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        for(auto l:lists){
+            if(l) q.push({l->val,l});
+        }
+        ListNode result;
+        ListNode *tail=&result;
+        while(!q.empty()){
+            auto p=q.top();
+            q.pop();
+            tail->next=p.p;
+            tail=tail->next;
+            if(p.p->next){
+                q.push({p.p->next->val,p.p->next});
+                if(q.size()==1) break;
+            }
+        }
+        return result.next;
+    }
+};
