3f131f33cb7c4837454888077baacfc50d3ae621
ScarboroughCoral <3249977074@qq.com>
2020-04-27 09:17:17 +0800

    feat:修改lint代码

--- LeetCode/23. 合并K个排序链表.cc
+++ LeetCode/23. 合并K个排序链表.cc
@@ -6,6 +6,15 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+#include <queue>
+#include <vector>
+using namespace std;
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
 class Solution
 {
   struct Node
