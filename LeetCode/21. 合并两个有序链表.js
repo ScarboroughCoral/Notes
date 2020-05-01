492a5cc28592c2cb61adb34c85af4883aa32c9e3
ScarboroughCoral <3249977074@qq.com>
2020-05-01 08:16:46 +0800

    feat:添加LeetCode第21题，打卡题目

---
+++ LeetCode/21. 合并两个有序链表.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+  let result = new ListNode();
+  let tail = result;
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      tail.next = l1;
+      l1 = l1.next;
+    } else {
+      tail.next = l2;
+      l2 = l2.next;
+    }
+    tail = tail.next;
+  }
+  if (l1) tail.next = l1;
+  if (l2) tail.next = l2;
+  return result.next;
+};
