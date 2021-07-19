e5e6a8c8f7c548689a461c008fdada3ccf070a46
Coral Lee <3249977074@qq.com>
2021-07-19 10:07:04 +0800

    Create 剑指 Offer 06. 从尾到头打印链表.ts

---
+++ 剑指Offer/剑指 Offer 06. 从尾到头打印链表.ts
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reversePrint(head: ListNode | null): number[] {
+    if (!head) return [];
+    return reversePrint(head.next).concat(head.val);
+};
