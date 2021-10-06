23662e0146451c72ef60e92e4be3126cb2688dcc
Mingyue Li <3249977074@qq.com>
2021-10-06 17:29:07 +0800

    Update 面试题 08.07. 无重复字符串的排列组合.ts

--- 程序员面试金典/面试题 08.07. 无重复字符串的排列组合.ts
+++ 程序员面试金典/面试题 08.07. 无重复字符串的排列组合.ts
@@ -15,3 +15,19 @@ function permutation(S: string): string[] {
     dfs('',new Set())
     return result;
 };
+function permutation1(S: string): string[] {
+    if (S.length > 1) {
+        const curChar = S[S.length - 1];
+        // 上一个排列组合单个元素字符串长度
+        const lastItemCount = S.length - 1;
+        const last = permutation(S.slice(0,lastItemCount));
+        const cur: string[] = [];
+        for (let s of last) {
+            for (let i = 0; i < lastItemCount + 1; i++) {
+                cur.push([...s.slice(0,i), curChar,...s.slice(i)].join(''));
+            }
+        }
+        return cur;
+    }
+    return [S[0]];
+};
