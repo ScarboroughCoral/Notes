bf3a67e78bc434589ce3c7d59a9f7137e571e2e7
Mingyue Li <3249977074@qq.com>
2021-10-10 12:36:41 +0800

    Update 35. 搜索插入位置.ts

--- LeetCode/35. 搜索插入位置.ts
+++ LeetCode/35. 搜索插入位置.ts
@@ -10,3 +10,18 @@ function searchInsert(nums: number[], target: number): number {
   }
   return result;
 }
+
+function searchInsert1(nums: number[], target: number): number {
+    let l = 0, r = nums.length - 1;
+    while(l <= r) {
+        let mid = l + Math.floor((r - l) / 2);
+        const midVal = nums[mid];
+        if (midVal >= target && mid === 0) return mid;
+        if (midVal < target && mid === nums.length - 1) return nums.length;
+        const midLastVal = nums[mid - 1];
+        if (midVal >= target && midLastVal < target) return mid;
+        if (midVal >= target) r = mid;
+        else l = mid + 1;
+    }
+    return -1;
+};
