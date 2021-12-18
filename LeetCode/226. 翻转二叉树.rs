140438b920e4e281875c08089aa64e27aeedd1ec
Mingyue Li <3249977074@qq.com>
2021-12-18 11:15:16 +0800

    Create 226. 翻转二叉树.rs

---
+++ LeetCode/226. 翻转二叉树.rs
@@ -0,0 +1,34 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        if let Some(node) = root.clone() {
+            let mut node = node.borrow_mut();
+            let left = node.left.take();
+            let right = node.right.take();
+            node.left = right.clone();
+            node.right = left.clone();
+            Self::invert_tree(left);
+            Self::invert_tree(right);
+        }
+        root
+    }
+}
