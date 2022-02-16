9ec834bda6a74cfa35bfe9d073f4a05c86c6dfd3
Mingyue Li <3249977074@qq.com>
2022-02-16 15:31:38 +0800

    Create 104. 二叉树的最大深度.rs

---
+++ LeetCode/104. 二叉树的最大深度.rs
@@ -0,0 +1,64 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::cmp;
+struct Meta {
+    pub result: i32,
+    pub depth: i32,
+}
+
+static mut meta: Meta = Meta {
+    result: 0,
+    depth: 1,
+};
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        unsafe {
+            meta = Meta {
+            result: 0,
+            depth: 1,
+            };
+        }
+        Solution::calc(root);
+        unsafe {
+            return meta.result;
+        }
+    }
+    fn calc(node: Option<Rc<RefCell<TreeNode>>>) {
+        if node == None {
+            return;
+        }
+        unsafe {
+            meta.result = cmp::max(meta.result, meta.depth);
+            meta.depth += 1;
+        }
+        if let Some(tmp) = node {
+            if let Some(left) = &tmp.borrow().left {
+                Solution::calc(Some(Rc::clone(&left)));
+            }
+            if let Some(right) = &tmp.borrow().right {
+                Solution::calc(Some(Rc::clone(&right)));
+            }
+        }
+        unsafe {
+            meta.depth -= 1;
+        }
+    }
+}
