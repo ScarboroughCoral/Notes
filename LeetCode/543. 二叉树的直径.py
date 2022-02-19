bb977965609b2fa8f89f4c4c9a2b03aeee1fb204
Mingyue Li <3249977074@qq.com>
2022-02-19 11:11:07 +0800

    Create 543. 二叉树的直径.py

---
+++ LeetCode/543. 二叉树的直径.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        diameter = 0
+        def depth(node: TreeNode) -> int:
+            if not node:
+                return 0
+            left = depth(node.left)
+            right = depth(node.right)
+            return max(left, right) + 1
+        def traverse(node: TreeNode):
+            nonlocal diameter
+            if not node:
+                return
+            left = depth(node.left)
+            right = depth(node.right)
+            diameter = max(diameter, left + right)
+            traverse(node.left)
+            traverse(node.right)
+        traverse(root)
+        return diameter
