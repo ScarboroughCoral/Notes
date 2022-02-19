f9521ac884055c4ead73a0da236e7744e1e951e0
Mingyue Li <3249977074@qq.com>
2022-02-19 11:33:41 +0800

    Update 543. 二叉树的直径.rs

--- LeetCode/543. 二叉树的直径.rs
+++ LeetCode/543. 二叉树的直径.rs
@@ -54,3 +54,51 @@ impl Solution {
     }
 
 }
+
+
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+static mut diameter: i32 = 0;
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::cmp;
+impl SolutionInfer {
+    pub fn diameter_of_binary_tree(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        unsafe {
+            diameter = 0;
+        }
+        Solution::depth(root.clone());
+        unsafe {
+            return diameter;
+        }
+    }
+    fn depth(node: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match node {
+            None => 0,
+            Some(node) => {
+                let left = Solution::depth(node.borrow().left.clone());
+                let right = Solution::depth(node.borrow().right.clone());
+                unsafe {
+                    diameter = cmp::max(left + right, diameter);
+                }
+                cmp::max(left, right) + 1
+            }
+        }
+    }
+}
