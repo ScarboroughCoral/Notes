cb8551740921f725d0ff6b479dcc959d043bc54b
Mingyue Li <3249977074@qq.com>
2022-02-19 11:26:55 +0800

    Update 543. 二叉树的直径.ts

--- LeetCode/543. 二叉树的直径.ts
+++ LeetCode/543. 二叉树的直径.ts
@@ -36,3 +36,31 @@ function diameterOfBinaryTree(root: TreeNode | null): number {
     traverse(root);
     return result;
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function diameterOfBinaryTreeInfer(root: TreeNode | null): number {
+    let diameter = 0;
+    const depth = (node: TreeNode | null): number => {
+        if(node === null) return 0;
+        const left = depth(node.left);
+        const right = depth(node.right);
+        diameter = Math.max(diameter, left + right);
+        return Math.max(left, right) + 1;
+    };
+    depth(root);
+    return diameter;
+};
