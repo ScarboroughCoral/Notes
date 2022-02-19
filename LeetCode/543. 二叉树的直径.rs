3c61e07442a621f3787971facd60822536fd8b49
Mingyue Li <3249977074@qq.com>
2022-02-19 11:19:47 +0800

    Create 543. 二叉树的直径.rs

---
+++ LeetCode/543. 二叉树的直径.rs
@@ -0,0 +1,56 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+static mut diameter: i32 = 0;
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::cmp;
+impl Solution {
+    pub fn diameter_of_binary_tree(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        unsafe {
+            diameter = 0;
+        }
+        Solution::traverse(root.clone());
+        unsafe {
+            return diameter;
+        }
+    }
+    fn traverse(node: Option<Rc<RefCell<TreeNode>>>) {
+        if let Some(node) = node {
+            let left = Solution::depth(node.borrow().left.clone());
+            let right = Solution::depth(node.borrow().right.clone());
+            let d = left + right;
+            unsafe {
+                diameter = cmp::max(d, diameter);
+            }
+            Solution::traverse(node.borrow().left.clone());
+            Solution::traverse(node.borrow().right.clone());
+        }
+    }
+    fn depth(node: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match node {
+            None => 0,
+            Some(node) => {
+                let left = Solution::depth(node.borrow().left.clone());
+                let right = Solution::depth(node.borrow().right.clone());
+                cmp::max(left, right) + 1
+            }
+        }
+    }
+
+}
