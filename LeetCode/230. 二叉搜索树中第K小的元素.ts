d49cad1e04b72dcf3ae449bbee80c9175959a315
Mingyue Li <3249977074@qq.com>
2022-02-23 16:09:28 +0800

    Create 230. 二叉搜索树中第K小的元素.ts

---
+++ LeetCode/230. 二叉搜索树中第K小的元素.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function kthSmallest(root: TreeNode | null, k: number): number {
+    let result = -1;
+    let i = 0;
+    const traverse = (node: TreeNode | null) => {
+        if (node === null || i >= k) return;
+        traverse(node.left);
+        i ++;
+        if (i === k) {
+            result = node.val
+            return
+        }
+        traverse(node.right);
+    };
+    traverse(root);
+    return result;
+};
